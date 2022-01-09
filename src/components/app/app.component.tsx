ed15acf4bb8c34d783cca1a81dcfbd86a7aa5683
pribrahimsari <pr.ibrahimsari@gmail.com>
2022-01-09 03:36:55 +0300

    gql

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -5,16 +5,20 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import 'common/styles';
 import About from "pages/about/about.page";
 import Home from "../../pages/home/home.page";
+import {ApolloProvider} from "@apollo/client";
+import client from "common/apollo-client";
 
 const App = () => {
     return(
-        <BrowserRouter>
+        <ApolloProvider client={client}>
+          <BrowserRouter>
             <Header />
             <Routes>
-                <Route path="/about" element={<About />} />
-                <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/" element={<Home />} />
             </Routes>
-        </BrowserRouter>
+          </BrowserRouter>
+        </ApolloProvider>
     );
 }
 
--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,28 +1,13 @@
 import PostsGrid from "../../components/posts-grid/posts-grid.component";
 import {IPost} from "../../common/interfaces/post.interface";
+import {useGetPosts} from "../../common/hooks/posts/useGetPosts";
 
 const Home = () => {
-    const posts:IPost[] = [
-        {
-            id: "1",
-            title: "Post One",
-            body: "Body of my post one"
-        },
-        {
-            id: "2",
-            title: "Post Two",
-            body: "Body of my post two"
-        },
-        {
-            id: "3",
-            title: "Post Three",
-            body: "Body of my post three"
-        },
-    ];
+    const posts = useGetPosts();
 
     return(
         <div className="home">
-            <PostsGrid posts={posts} />
+            <PostsGrid posts={posts || []} />
         </div>
     );
 }
